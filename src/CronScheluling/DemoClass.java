1a53146f61b700667587134af9076aeb069127f3
vickydeveloper <vickyroman404@gmail.com>
2024-01-10 15:28:12 +0530

    added new classes

---
+++ src/CronScheluling/DemoClass.java
@@ -0,0 +1,8 @@
+package CronScheluling;
+
+public class DemoClass {
+
+    public static void main(String[] args) {
+        System.out.println("Added Demo class for testing git commands");
+    }
+}
