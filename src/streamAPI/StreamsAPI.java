8967602aa4813917c63af597054191f2f95aad33
vigneshs173 <vickyroman404@gmail.com>
2024-01-19 14:26:22 +0530

    added new strams again

--- src/streamAPI/StreamsAPI.java
+++ src/streamAPI/StreamsAPI.java
@@ -1,19 +1,20 @@
 package streamAPI;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class StreamsAPI {
     public static void main(String[] args) {
-//
-//        List<String> list = Arrays.asList("a1", "a2", "b1", "c2", "c1");
-//        list.stream()
-//                .filter(s -> s.startsWith("c"))
-//                .map(String::toUpperCase)
-//                .sorted(String::compareTo)
-//                .forEach(System.out::println);
+
+        List<String> list = Arrays.asList("zzzzzz","a1", "a2", "b1", "c2", "c1","c","a");
+        list.stream()
+                .filter(s -> s.endsWith("c") || s.endsWith("a"))
+                .map(String::toUpperCase)
+                .sorted(Comparator.reverseOrder())
+                .forEach(System.out::println);
 
 
         int[] arr = {11,44,22,77,55,77,89,90};
@@ -22,5 +23,8 @@ public class StreamsAPI {
 //        Arrays.stream(arr).forEach(s -> System.out.println(s));
         Stream<Object> att=Arrays.stream(arr2).map(a->a.toUpperCase());
         System.out.println(Arrays.toString(att.toArray()));
+
+
+
     }
 }
