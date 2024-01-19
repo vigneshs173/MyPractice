d7065e33d62cb50a5fe6a788974b02c8b2e10ea4
vigneshs173 <vickyroman404@gmail.com>
2024-01-19 14:21:09 +0530

    added streams api

--- src/streamAPI/StreamsAPI.java
+++ src/streamAPI/StreamsAPI.java
@@ -2,15 +2,25 @@ package streamAPI;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StreamsAPI {
     public static void main(String[] args) {
+//
+//        List<String> list = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+//        list.stream()
+//                .filter(s -> s.startsWith("c"))
+//                .map(String::toUpperCase)
+//                .sorted(String::compareTo)
+//                .forEach(System.out::println);
 
-        List<String> list = Arrays.asList("a1", "a2", "b1", "c2", "c1");
-        list.stream()
-                .filter(s -> s.startsWith("c"))
-                .map(String::toUpperCase)
-                .sorted(String::compareTo)
-                .forEach(System.out::println);
+
+        int[] arr = {11,44,22,77,55,77,89,90};
+        String[] arr2 = {"aaa","sss","fsf", "kljjf"};
+
+//        Arrays.stream(arr).forEach(s -> System.out.println(s));
+        Stream<Object> att=Arrays.stream(arr2).map(a->a.toUpperCase());
+        System.out.println(Arrays.toString(att.toArray()));
     }
 }
